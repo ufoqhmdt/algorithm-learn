feafffe964871b989a63835765195d050838674b
UFO <ufoqhmdt@gmail.com>
2017-06-23 23:30:01 +0800

    fix

--- ipParse/src/ipStrParse.js
+++ ipParse/src/ipStrParse.js
@@ -2,60 +2,73 @@ const IPError = require('./IPError');
 
 function parseIp(ip) {
   try {
+    if (!ip) {
+      throw new IPError();
+    }
     const items = ip.split('.');
     if (items.length !== 4) {
-      throw new IPError('length error');
+      throw new IPError();
     }
-    console.log(items);
+
+    // console.log(items);
     const result = items.reduce((accumulator, currentValue, currentIndex, array) => {
-      const currentValueTrim = currentValue.trim();
-      if (currentValueTrim.indexOf(' ') !== -1) {
-        throw new IPError();
-      }
-      let currentValueTrimInt = Number.parseInt(currentValueTrim);
-      checkScope(currentValueTrimInt);
+      // console.log('accumulator', accumulator, 'currentValue', currentValue, currentIndex, '--');
+      const acc = accumulator.trim();
+      const cur = currentValue.trim();
+      let curInt = Number.parseInt(cur);
+
+      checkException(acc, cur);
+      checkScope(curInt);
 
       // let accumulator2 = null;
       // if (typeof accumulator === 'string') {
       //   accumulator2 = parseInt(accumulator.toString().trim());
       // }
 
-      console.log(currentValueTrimInt,'<<<')
+      // console.log(curInt, '<<<')
       if (currentIndex === 1) {
         checkScope(accumulator);
-        console.log('1---')
+        // console.log('1---')
         accumulator = transfer(accumulator);
-        currentValueTrimInt = transfer(currentValueTrimInt);
+        curInt = transfer(curInt);
       } else {
-        currentValueTrimInt = transfer(currentValueTrimInt);
+        curInt = transfer(curInt);
       }
-      console.log('accumulator', accumulator, 'currentValue', currentValueTrimInt, currentIndex, '--');
-      return accumulator.concat(currentValueTrimInt);
+      // console.log('accumulator', accumulator, 'currentValue', curInt, currentIndex, '--');
+      return accumulator.concat(curInt);
     });
-    return Number.parseInt(result,2);
+    return Number.parseInt(result, 2);
   } catch (e) {
     throw new Error(e);
   }
 }
 
 function transfer(val) {
-  console.log('>>>1',val);
+  // console.log('>>>1', val);
   val = parseInt(val).toString(2);
-  console.log('>>>2',val);
+  // console.log('>>>2', val);
   if (val.length < 8) {
     const zeroNumber = 8 - val.length;
     for (var i = 0; i < zeroNumber; i++) {
-      val='0'.concat(val);
+      val = '0'.concat(val);
     }
   }
-  console.log('>>>3',val);
+  // console.log('>>>3', val);
   return val
 }
 
-function checkScope(val){
+function checkException(acc, cur) {
+
+  if (cur.indexOf(' ') !== -1 || acc.indexOf(' ') !== -1) {
+    throw new IPError();
+  }
+}
+
+function checkScope(val) {
+
   if (val < 0 || val > 255) {
-        throw new IPError('must be between 0 and 255');
-      }
+    throw new IPError();
+  }
 }
 
 module.exports = parseIp;
--- ipParse/test/ipStrParse.test.js
+++ ipParse/test/ipStrParse.test.js
@@ -31,7 +31,7 @@ describe('test ip parse', function() {
   });
 
   it('运算结果的值匹配', function() {
-    console.log(ipStrParse('172.18.199.12'),'00000000');
+    console.log(ipStrParse('172.18.199.12'), '00000000');
     expect(ipStrParse('172.18.199.12')).to.equal(2886911756);
   });
 
@@ -44,36 +44,142 @@ describe('test ip parse', function() {
   it('异常测试：长短必须为4', function() {
     assert.throws(function() {
       ipStrParse('172.12');
-    }, 'Error: length error');
+    }, 'Error: is invalid input');
   });
 
   it('异常测试：长短必须为4', function() {
     assert.throws(function() {
       ipStrParse('172.12.12.23.33');
-    }, 'Error: length error');
+    }, 'Error: is invalid input');
   });
 
   it('异常测试：每一个item都应该在0到255之间', function() {
     assert.throws(function() {
       ipStrParse('172.12.211.290');
-    }, 'Error: must be between 0 and 255');
+    }, 'is invalid input');
   });
 
   it('异常测试：每一个item都应该在0到255之间', function() {
     assert.throws(function() {
       ipStrParse('-172.12.211.22');
-    }, 'Error: must be between 0 and 255');
+    }, 'is invalid input');
   });
 
   it('异常测试：每一个item都应该在0到255之间', function() {
     assert.throws(function() {
       ipStrParse('72.-12.211.22');
-    }, 'Error: must be between 0 and 255');
+    }, 'is invalid input');
   });
   it('异常测试：每一个item都应该在0到255之间', function() {
     assert.throws(function() {
       ipStrParse('72.12.211.-22');
-    }, 'Error: must be between 0 and 255');
+    }, 'is invalid input');
   });
+  it('ERROR', function() {
+    assert.throws(function() {
+      ipStrParse("17 2.168.5.1");
+    }, 'Error: is invalid input');
+  });
+
+
+  // it('ERROR', function() {
+  //   assert.throws(function() {
+  //     ipStrParse('72.12.211.-22');
+  //   }, 'is invalid input');
+  // });
+
+
+
+  // it('ERROR', function() {
+  //   assert.throws(function() {
+  //     ipStrParse(NULL);
+  //   }, 'is invalid input');
+  // });
+  // it('ERROR', function() {
+  //   assert.throws(function() {
+  //     ipStrParse("");
+  //   }, 'is invalid input');
+  // });
+  // it('ERROR', function() {
+  //   assert.throws(function() {
+  //     ipStrParse("fdsafdsa");
+  //   }, 'is invalid input');
+  // });
+  // it('ERROR', function() {
+  //   assert.throws(function() {
+  //     ipStrParse("17x.168.5.1");
+  //   }, 'is invalid input');
+  // });
+  // it('ERROR', function() {
+  //   assert.throws(function() {
+  //     ipStrParse("9299.168.5.1");
+  //   }, 'is invalid input');
+  // });
+  // it('ERROR', function() {
+  //   assert.throws(function() {
+  //     ipStrParse("1172.1629.5.1");
+  //   }, 'is invalid input');
+  // });
+  // it('ERROR', function() {
+  //   assert.throws(function() {
+  //     ipStrParse("172.12689.5.1");
+  //   }, 'is invalid input');
+  // });
+  // it('ERROR', function() {
+  //   assert.throws(function() {
+  //     ipStrParse("172.1689.5.1.9");
+  //   }, 'is invalid input');
+  // });
+  // it('ERROR', function() {
+  //   assert.throws(function() {
+  //     ipStrParse("172.168.-8.1");
+  //   }, 'is invalid input');
+  // });
+  // it('ERROR', function() {
+  //   assert.throws(function() {
+  //     ipStrParse(".172.168.5.1");
+  //   }, 'is invalid input');
+  // });
+  // it('ERROR', function() {
+  //   assert.throws(function() {
+  //     ipStrParse("172.168.1.");
+  //   }, 'is invalid input');
+  // });
+  // it('ERROR', function() {
+  //   assert.throws(function() {
+  //     ipStrParse("172.168.5.1.");
+  //   }, 'is invalid input');
+  // });
+  // it('ERROR', function() {
+  //   assert.throws(function() {
+  //     ipStrParse("172.168.5.");
+  //   }, 'is invalid input');
+  // });
+  // it('ERROR', function() {
+  //   assert.throws(function() {
+  //     ipStrParse("172..168.5");
+  //   }, 'is invalid input');
+  // });
+  // it('ERROR', function() {
+  //   assert.throws(function() {
+  //     ipStrParse("123456");
+  //   }, 'is invalid input');
+  // });
+  // it('ERROR', function() {
+  //   assert.throws(function() {
+  //     ipStrParse("-1");
+  //   }, 'is invalid input');
+  // });
+
+
+
+  // testResult("192.168.1.1", 192 << 24 | 168 << 16 | 1 << 8 | 1, S_OK);
+  //  testResult("0.128.2.1", 0  << 24 | 128 << 16 | 2 << 8 | 1, S_OK);
+  //  testResult("0192.000168.1.1", 192 << 24 | 168 << 16 | 1 << 8 | 1, S_OK);
+  //  testResult("172.168.5.1", 172 << 24 | 168 << 16 | 5 << 8 | 1, S_OK);
+  //  testResult("172.  168.5.1", 172 << 24 | 168 << 16 | 5 << 8 | 1, S_OK);
+  //  testResult("172.168  .5.1", 172 << 24 | 168 << 16 | 5 << 8 | 1, S_OK);
+  //  testResult("  172.168 . 5 .  1 ", 172 << 24 | 168 << 16 | 5 << 8 | 1, S_OK);
+
 
 });
